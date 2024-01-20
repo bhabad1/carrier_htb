dcc0a8a49541c52b37158cf2235ff0e7658c8344
bhabad1 <bhabad1@gmail.com>
2024-01-20 13:12:05 +0530

    skeleton for the basic functions

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var logger = require("morgan");
 var cors = require("cors");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var usersRouter = require("./server/routes/users");
 
 var app = express();
 
