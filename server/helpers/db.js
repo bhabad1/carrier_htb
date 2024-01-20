934531705b7de2dd45ef85eba82447191f65bae6
bhabad1 <bhabad1@gmail.com>
2024-01-20 15:29:21 +0530

    db config refactored

--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,1 +1,17 @@
 //database connection logic will go here
+const mysql = require("mysql2");
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "metrics",
+});
+db.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+module.exports = db;
