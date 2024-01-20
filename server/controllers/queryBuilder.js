3526f9aa4c6b1284425a214e4f7cfbeaf5423fad
bhabad1 <bhabad1@gmail.com>
2024-01-20 17:10:54 +0530

    no changes

--- server/controllers/queryBuilder.js
+++ server/controllers/queryBuilder.js
@@ -3,7 +3,15 @@ let db = require("../helpers/db");
 
 queryBuilder.getAllOperands = async (req, res, next) => {
   try {
-  } catch (error) {}
+    db.query("SELECT * FROM expressions", (err, results) => {
+      console.log(results + "========rre");
+      if (err) throw err;
+      res.json(results);
+    });
+  } catch (error) {
+    console.log("Error: ", error);
+    next(error);
+  }
 };
 
 module.exports = queryBuilder;
